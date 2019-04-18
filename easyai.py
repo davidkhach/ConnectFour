dc639f266eb8f52cf67dadd71fd5a9b65c38034c
unknown <dkhachatryan3@gmail.com>
2019-04-18 13:31:06 -0700

    Started easy AI class

---
+++ easyai.py
@@ -0,0 +1,13 @@
+import connectfour
+
+class EasyAI():
+	def __init__(self):
+		pass
+
+	def chooseMove(self):
+		pass
+
+	def search(self, gameState, depth, turn):
+		pass
+	def evaluate(self, gameState, turn):
+		pass

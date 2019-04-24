c69f94cddbe8d3a7e84ce7dfa01b24f5d3cb585e
unknown <dkhachatryan3@gmail.com>
2019-04-24 15:49:00 -0700

    Added start menu, user can choose to play another player or an AI(AI is easy difficulty by default for now, options to change difficulty coming soon)

--- view.py
+++ view.py
@@ -10,14 +10,28 @@ def displayStartScreen():
 	screen = pygame.display.set_mode((width, height))
 	pygame.display.set_caption('Connect Four')
 	screen.fill(background_colour)
+	pygame.draw.rect(screen, (255,0,0), [70, 450, 450, 100])
 	startMessage(screen, 1200, 900)
 	playHumanButton(screen, 1200, 900)
 	playAIButton(screen, 1200, 900)
 	running = True
+	pos = None
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
+			elif event.type == pygame.MOUSEBUTTONUP:
+				pos = pygame.mouse.get_pos()
+		if pos != None:
+			xpos = pos[0]
+			ypos = pos[1]
+			print (pos)
+			if (xpos > 69 and xpos < 519 and ypos > 451 and ypos < 550):
+				return "HUMAN"
+			elif (xpos > 748 and xpos < 1149 and ypos > 451 and ypos < 550):
+				return "AI"
+				
+			
 		pygame.display.flip()
 
 def startMessage(screen, display_width, display_height):
@@ -27,9 +41,15 @@ def startMessage(screen, display_width, display_height):
 	TextRect.center = ((display_width/2),(display_height-700))
 	screen.blit(TextSurf, TextRect)
 
+	largeText2 = pygame.font.Font(pygame.font.get_default_font(), 20)
+	TextSurf2, TextRect2 = text_objects("(Click which gamemode you want to play!)", largeText2)
+	TextRect2.center = ((display_width/2),(display_height-640))
+	screen.blit(TextSurf2, TextRect2)
+
 	pygame.display.update()
 
 def playHumanButton(screen, display_width, display_height):
+	pygame.draw.rect(screen, (0,255,0), [750, 450, 400, 100])
 	pygame.font.init()
 	largeText = pygame.font.Font(pygame.font.get_default_font(), 50)
 	TextSurf, TextRect = text_objects("Player vs Player", largeText)
@@ -44,8 +64,7 @@ def playAIButton(screen, display_width, display_height):
 	TextRect.center = ((display_width-250),(display_height-400))
 	screen.blit(TextSurf, TextRect)
 
-
-def createGrid():
+def createGrid(gameMode):
 	background_colour = (0,0,0)
 	(width, height) = (1200, 900)
 	screen = pygame.display.set_mode((width, height))
@@ -68,10 +87,11 @@ def createGrid():
 	pygame.draw.rect(screen, (0,0,255), [width-270,0,line_width, height-70])
 	running = True
 	welcomeMessage(screen, width,height)
+	gameTypeMessage(screen, width, height, gameMode)
 	game = startGame()
 	pos = None
-	ai = easyai.EasyAI((255,0,0), game)
-	#ai = mediumai.MediumAI("R", game)
+	if (gameMode == "AI"):
+		ai = easyai.EasyAI((255,0,0), game)
 	for column in range(0+margin,720,w+margin):
 		for row in range(0+margin,840,h+margin):
 			pygame.draw.circle(screen, (255,255,255), (row +70, column + 80), 60, 0)
@@ -94,16 +114,17 @@ def createGrid():
 		
 		if (turn == "R"):
 			#ai.setBoard(game.returnBoard())
-			ai.setBoard(game)
-			displayCurrentTurn(screen, turn)
 			color = (255, 0, 0)
-			move = ai.chooseMove()
+			displayCurrentTurn(screen, turn)
+			if (gameMode == "AI"):
+				ai.setBoard(game)
+				move = ai.chooseMove()
 	
-			coordinates = game.makeMove(move+1, turn)
+				coordinates = game.makeMove(move+1, turn)
 
 
 
-			dropDiskSlowly(screen, move, color,coordinates, clock)
+				dropDiskSlowly(screen, move, color,coordinates, clock)
 
 
 		else:
@@ -125,14 +146,25 @@ def createGrid():
 					if event.type == pygame.QUIT:
 						quit = True
 			running = False
-
-				# x goes from 22 to 136 for first column, + 130 to 22 and 136 for next column, Y ranges from: 21 to 800
 		
 		displayCurrentTurn(screen, turn)
 
 
 		pygame.display.flip()
+	return 0
 
+def gameTypeMessage(screen, display_width, display_height, gameType):
+	if (gameType == "HUMAN"):
+		result = "Player"
+	else:
+		result = "AI"
+	pygame.font.init()
+	largeText = pygame.font.Font(pygame.font.get_default_font(), 20)
+	TextSurf, TextRect = text_objects("(Player vs " + result + ")", largeText)
+	TextRect.center = ((display_width-130),(display_height-750))
+	screen.blit(TextSurf, TextRect)
+
+	pygame.display.update()
 
 def startGame():
 	playGame = connectfour.ConnectFour()
@@ -212,5 +244,9 @@ def welcomeMessage(screen, display_width, display_height):
 
 
 
-createGrid()
-#displayStartScreen()
+#createGrid()
+result = displayStartScreen()
+if result == "AI":
+	createGrid("AI")
+else:
+	createGrid("HUMAN")

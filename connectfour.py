ac7d491b5c6e0ecb880c8341c87d03dd418d5f25
unknown <dkhachatryan3@gmail.com>
2019-05-01 17:56:54 -0700

    Added docstring comments to ConnectFour class

--- connectfour.py
+++ connectfour.py
@@ -1,5 +1,15 @@
+# ConnectFour Game Class
+# Implemented the logic for dopping the disks in valid columns/rows
+# Implemented detecting winner if 4-in-a-row of the same disk horizontally/vertically/diagonally.
+# Can play the game in console by calling playConsoleGame() method.
+
+
+
+
+from copy import deepcopy
 class ConnectFour():
 	def __init__(self):
+		""" Initializes the class object, sets up various helper member variables """
 		self.board = []
 		self.rows = 6
 		self.columns = 7
@@ -8,30 +18,42 @@ class ConnectFour():
 		self.currentPlayer = "R"
 
 	def createBoard(self):
+		""" Creates a two-dimensional empty board and assigns said board to self.board member variable """
 		for r in range(self.rows):
 			self.board.append([])
 			for c in range(self.columns):
 				self.board[r].append("E")
 
 	def returnBoard(self):
+		""" Returns two-dimensional game board """ 
 		return self.board
 	def swapBoard(self, anotherBoard):
-		self.board = anotherBoard
+		""" Swaps out board for a copy of another board """
+		self.board = deepcopy(anotherBoard)
 
 	def printBoard(self):
+		""" Prints out the board in an easy to see manner in the console """
 		for r in range(self.rows):
 			print(self.board[r])
 
-	def checkDiskCount():
+	def checkDiskCount(self):
+		""" Returns total disks, useful for checking for draws """
 		return self.diskCount
 
-	def checkGameOver():
+	def checkGameOver(self):
+		""" Returns whether game is over """
 		return self.game_over
 
 	def currentTurn(self):
+		""" Returns the current player's turn """
 		return self.currentPlayer
 
+	def setTurn(self, turn):
+		""" Sets the current players turn """
+		self.currentPlayer = turn
+
 	def makeMove(self, column, turn):
+		""" Drops a disk on the board if column choice is valid """
 		if (self.isValid(column)):
 			locationCoord = self.findPlace(column)
 			self.board[locationCoord[0]][locationCoord[1]] = turn
@@ -43,15 +65,16 @@ class ConnectFour():
 			return locationCoord
 		else:
 			return None
-			#raise IncorrectMove("Please select a valid column to drop your disc")
 
 	def findPlace(self, column):
+		""" Finds the place to drop disk on the board, returns coordinates to that place """
 		for i in range(self.rows-1, -1 , -1):
 			if (self.board[i][column-1] == "E"):
 				return (i, column-1)
 
 
 	def isValid(self, column):
+		""" Returns whether column number to drop disk in is valid """
 		if (column > 8 or column < 1):
 			return False
 		elif (self.board[0][column-1] != "E"):
@@ -59,6 +82,7 @@ class ConnectFour():
 		return True
 
 	def playConsoleGame(self):
+		""" Allows user to play ConnectFour with another player in the console """
 		self.currentPlayer = "R"
 		winner = ""
 		self.printBoard()
@@ -83,9 +107,11 @@ class ConnectFour():
 
 
 	def checkWin(self, turn):
+		""" Returns whether a player wins or not """
 		return self.checkHorizontal(turn) or self.checkVertical(turn) or self.checkDiagonalTopToBottom(turn) or self.checkDiagonalBottomToTop(turn)
 
 	def checkHorizontal(self, turn):
+		""" Checks if the current player matches 4 in a row horizontally anywhere in the board """
 		for r in self.board:
 			count = 0
 			for val in r:
@@ -98,6 +124,7 @@ class ConnectFour():
 		return False
 
 	def checkVertical(self, turn):
+		""" Checks if the current player matches 4 in a row vertically anywhere in the board """
 		safetyCheck = 0
 		for r in range(self.rows):
 			for c in range(self.columns):
@@ -108,6 +135,7 @@ class ConnectFour():
 		return False
 
 	def checkDiagonalTopToBottom(self, turn):
+		""" Checks if the current player matches 4 in a row diagonally from top to bottom (\) anywhere in the board """
 		for r in range(self.rows-3):
 			for c in range(self.columns-3):
 				if (self.board[r][c] == turn and self.board[r+1][c+1] == turn and self.board[r+2][c+2] == turn and self.board[r+3][c+3] == turn):
@@ -115,6 +143,7 @@ class ConnectFour():
 		return False
 
 	def checkDiagonalBottomToTop(self, turn):
+		""" Checks if the current player matches 4 in a row diagonally from bottom to top (/) anywhere in the board """
 		for r in range(self.rows-1, 2, -1):
 			for c in range(self.columns-3):
 				if (self.board[r][c] == turn and self.board[r-1][c+1] == turn and self.board[r-2][c+2] == turn and self.board[r-3][c+3] == turn):
@@ -130,4 +159,5 @@ class ConnectFour():
 
 
 class IncorrectMove(Exception):
+	""" Exception class for invalid moves """
 	pass

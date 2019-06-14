6b831edd0ee202977ef8a152b42348f658010ff5
unknown <dkhachatryan3@gmail.com>
2019-06-14 13:19:37 -0700

    Fixed bugs

--- easyai.py
+++ easyai.py
@@ -8,10 +8,11 @@ import connectfour
 import random
 
 class EasyAI():
-	def __init__(self, turn, gameBoard):
+	def __init__(self, turn, game):
 		""" Initializes what color the AI is playing as and stores the board information """
 		self.turn = turn
-		self.gameBoard = gameBoard
+		self.game = game
+		self.gameBoard = game.returnBoard() 
 
 	def setBoard(self, board):
 		""" Swaps out the current gameboard with another """
@@ -21,6 +22,7 @@ class EasyAI():
 		""" Returns a random column to drop disk in as long as it is valid (not full) """
 		listOfColumns = [0,1,2,3,4,5,6]
 		result = random.choice(listOfColumns)
-		while (self.gameBoard.isValid(result+1) != True):
+		
+		while (self.game.isValid(result+1) != True):
 			result = random.choice(listOfColumns)
 		return result
